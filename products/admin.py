6a3f1619030adfdf1101f7d7c437f86760c05caf
Andrey Statsenko <smooker14@gmail.com>
2015-09-22 16:12:05 +0300

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from products import models
 
 # Register your models here.
+
+admin.site.register(models.Item)
+admin.site.register(models.Category)
+admin.site.register(models.Comment)
+admin.site.register(models.Rate)
