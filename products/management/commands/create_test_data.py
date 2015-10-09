2212e573eddc15b31cc769e1cfb974c4d5b48993
jeezybrick <smooker14@gmail.com>
2015-10-09 10:15:59 +0300

    add random choice on category test data

--- products/management/commands/create_test_data.py
+++ products/management/commands/create_test_data.py
@@ -11,16 +11,13 @@ def random_string(length=100):
     return u''.join(random.choice(string.ascii_letters) for x in range(length))
 
 
-def random_email(length=10):
-    return u''.join(random.choice(string.ascii_letters) for x in range(length))
-
-
-def random_url(length=6):
-    return u''.join(random.choice(string.ascii_letters) for x in range(length))
-
-
 def random_price(fromm, to):
     return random.randint(fromm, to)
+
+
+def random_parent():
+    choice = [1, '']
+    return random.choice(choice)
 
 
 class Command(BaseCommand):
@@ -31,10 +28,11 @@ class Command(BaseCommand):
             model = models.MyUser
 
         username = factory.LazyAttribute(lambda t: random_string(length=10))
-        email = factory.LazyAttribute(lambda t: random_email()+'@gmail.com')
+        email = factory.LazyAttribute(lambda t: random_string()+'@gmail.com')
         password = factory.LazyAttribute(lambda t: random_string(length=10))
 
     users = UserFactory.create_batch(10)
+    print('Wait.Users create...')
     [user.save() for user in users]
 
     """Create categories"""
@@ -43,9 +41,10 @@ class Command(BaseCommand):
             model = models.Category
 
         name = factory.LazyAttribute(lambda t: random_string(length=10))
-        parent_category = None
+        parent_category_id = factory.LazyAttribute(lambda t: random_parent())
 
     categories = CategoryFactory.create_batch(50)
+    print('Wait.Categories create...')
     [category.save() for category in categories]
 
     """Create items"""
@@ -55,12 +54,13 @@ class Command(BaseCommand):
 
         name = factory.Sequence(lambda n: 'item-{0}'.format(n))
         price = factory.LazyAttribute(lambda t: random_price(1, 10000))
-        image_url = factory.LazyAttribute(lambda t: 'https://' + random_url())
+        image_url = factory.LazyAttribute(lambda t: 'https://' + random_string() + '/' + random_string() + '/')
         description = factory.LazyAttribute(lambda t: random_string())
         user_id = 1
         quantity = factory.LazyAttribute(lambda t: random_price(0, 50))
 
     items = ItemFactory.create_batch(100)
+    print('Wait.Items create...')
     [item.save() for item in items]
 
     """Create comments"""
@@ -74,6 +74,7 @@ class Command(BaseCommand):
         item_id = factory.LazyAttribute(lambda t: random_price(5, 50))
 
     comments = CommentFactory.create_batch(100)
+    print('Wait.Comments create...')
     [comment.save() for comment in comments]
 
     def handle(self, *args, **options):
