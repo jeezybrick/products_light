0c7c4b47925e426a0824dbb245ec7d263b2fb07c
jeezybrick <smooker14@gmail.com>
2015-10-16 09:34:25 +0300

    split admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
 from products import models
-from cart.models import Cart
-from categories.models import Category
 
 # Register your models here.
 
-admin.site.register(models.MyUser)
 admin.site.register(models.Item)
-admin.site.register(Category)
 admin.site.register(models.Comment)
 admin.site.register(models.Rate)
-admin.site.register(Cart)
 admin.site.register(models.Action)
