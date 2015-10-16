5c043e2c994222413e835159e2bae9fc148441a3
jeezybrick <smooker14@gmail.com>
2015-10-16 17:12:47 +0300

    django admin bootstrap

--- test_case/settings.py
+++ test_case/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    # django admin-bootstrap
+    'django_admin_bootstrapped',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
